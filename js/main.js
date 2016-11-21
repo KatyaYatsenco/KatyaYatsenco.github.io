67de52954dc7d53394f8bdf11cb02b819de60a52
KatyaYatsenco <k.yatsenco@gmail.com>
2016-11-21 19:18:24 +0200

    Added switching between 3 categories.

--- js/main.js
+++ js/main.js
@@ -1,32 +1,13 @@
-// var openMenuBar = document.querySelector('#menuImg');
-// var navBar = document.querySelector('#navList');
-//
-//     openMenuBar.addEventListener('click', function() {
-//         var style = window.getComputedStyle(navBar);
-//         // console.log(style.visibility);
-//         if (style.visibility === 'hidden') {
-//             navBar.style.visibility = 'visible';
-//             openMenuBar.src = '../image/close_menu.png';
-//         }else {
-//             navBar.style.visibility = 'hidden';
-//             openMenuBar.src = '../image/menu.png';
-//         }
-//
-//
-//     });
-
-
-
+//Navigation menu
 var menuImg = document.getElementById('menu_img_box');
 var navBar = document.querySelector('#navList');
 var menuImg1 = document.getElementById('menu_img1');
 var menuImg2 = document.getElementById('menu_img2');
 var menuImg3 = document.getElementById('menu_img3');
 
-
 menuImg.addEventListener('click', function () {
-   var style = window.getComputedStyle(navBar);
-    if(style.visibility === 'hidden') {
+    var style = window.getComputedStyle(navBar);
+    if (style.visibility === 'hidden') {
         navBar.style.visibility = 'visible';
         menuImg1.classList.add('rotate_img1');
         menuImg2.classList.add('visibility_hidden');
@@ -39,3 +20,36 @@ menuImg.addEventListener('click', function () {
         menuImg3.classList.remove('rotate_img3');
     }
 });
+
+//Gallery Menu
+var galleryMenuCategories = document.getElementsByClassName('menuCategories');
+var galleryMenuPageBackground = document.getElementsByClassName('main_background')[0];
+// debugger;
+
+var category;
+for (var i = 0; i < galleryMenuCategories.length; i++) {
+    category = galleryMenuCategories[i];
+    var categoryMouseover = category.addEventListener('mouseover', function categoryInFocus() {
+        var findLindCurrentCategory = this.getElementsByTagName('a')[0];
+        findLindCurrentCategory.style.display = 'block';
+        if(findLindCurrentCategory.id === 'section3D') {
+            galleryMenuPageBackground.style.backgroundImage = 'url("../image/gallery_background_3d.png")';
+        }
+        else {
+            galleryMenuPageBackground.style.backgroundImage = 'url("../image/main_background.jpg")';
+        }
+
+    });
+    var categoryMouseoout = category.addEventListener('mouseout', function categoryInFocus() {
+        var findLindCurrentCategory = this.getElementsByTagName('a')[0];
+        findLindCurrentCategory.style.display = 'none';
+        if(findLindCurrentCategory.id === 'section3D') {
+            galleryMenuPageBackground.style.backgroundImage = 'url("../image/main_background.jpg")';
+        }
+    })
+}
+
+
+
+
+
