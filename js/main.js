48cdff77e678f88ef786a1ee7b9a9d7e0dfecf6f
KatyaYatsenco <k.yatsenco@gmail.com>
2016-12-04 17:08:41 +0200

    version 0.1

--- js/main.js
+++ js/main.js
@@ -21,42 +21,130 @@ menuImg.addEventListener('click', function () {
     }
 });
 
-//Gallery Menu
-var galleryMenuCategories = document.getElementsByClassName('menuCategories');
-var galleryMenuPageBackground = document.getElementsByClassName('main_background')[0];
-// debugger;
+// open target category
+var galleryMenuLinks = document.querySelectorAll('.link');
+var svgSliceOfWood = document.querySelector('#svgSliceOfWood');
+var mainContent = document.querySelector('.position_center_flex');
+var containerContents = document.querySelector('.container_contents');
+var projects = document.querySelector('.projects');
+var arr = [];
+for (var i = 0; i < galleryMenuLinks.length; i++) {
+    var link = galleryMenuLinks[i];
 
-var category;
-for (var i = 0; i < galleryMenuCategories.length; i++) {
-    category = galleryMenuCategories[i];
-    var categoryMouseover = category.addEventListener('mouseover', function categoryInFocus() {
-        var findLindCurrentCategory = this.getElementsByTagName('a')[0];
-        findLindCurrentCategory.style.display = 'block';
-        if(findLindCurrentCategory.id === 'section3D') {
-            galleryMenuPageBackground.style.backgroundImage = 'url("../image/gallery_background_3d.png")';
+    arr.push(link);
+    link.addEventListener('click', function () {
+        var target = this;
+        if (!newDiv) {
+            var newDiv = document.createElement('div');
         }
-        else if(findLindCurrentCategory.id === 'sectionPainting') {
-            galleryMenuPageBackground.style.backgroundImage = 'url("../image/gallery_background_painting.png")';
+        newDiv.setAttribute('id', 'titleGalleryDivision');
+        containerContents.insertBefore(newDiv, projects);
+        target.setAttribute('class', 'positionStatic');
+
+        arr.forEach(function (item) {
+            if (item === target) {
+                newDiv.appendChild(target);
+                target.setAttribute('class', 'styleTitleGallery');
+                svgSliceOfWood.style.display = 'none';
+                projects.style.display = 'flex';
+            }
+            else {
+                item.style.display = 'none';
+            }
+        });
+    });
+}
+
+
+// Open project
+var projectsImg = document.querySelectorAll('.project_img');
+// function openProject() {
+var arrProjectsImg = [];
+for (var k = 0; k < projectsImg.length; k++) {
+    var projectImg = projectsImg[k];
+    arrProjectsImg.push(projectImg);
+}
+
+arrProjectsImg.forEach(function (item) {
+    console.log(item);
+    item.addEventListener('click', function () {
+        var target = this;
+        var targetChild = target.childNodes[3];
+        if (item === target) {
+            targetChild.style.width = '900px';
         }
         else {
-            galleryMenuPageBackground.style.backgroundImage = 'url("../image/main_background.jpg")';
-
+            item.style.display = 'none';
+            // console.log(item);
         }
-
     });
-    var categoryMouseoout = category.addEventListener('mouseout', function categoryInFocus() {
-        var findLindCurrentCategory = this.getElementsByTagName('a')[0];
-        findLindCurrentCategory.style.display = 'none';
-        if(findLindCurrentCategory.id === 'section3D') {
-            galleryMenuPageBackground.style.backgroundImage = 'url("../image/main_background.jpg")';
+});
 
-        }
-        else if(findLindCurrentCategory.id === 'sectionPainting') {
-            galleryMenuPageBackground.style.backgroundImage = 'url("../image/main_background.jpg")';
+// Navigation Links About Me
+var navigationLinksAboutMe = document.querySelector('#navigationLinkAboutMe');
+var aboutMeInfo = document.querySelector('.aboutMeInfo');
+navigationLinksAboutMe.addEventListener('click', function () {
+    mainContent.style.display = 'none';
+    if (!newDiv) {
+        var newDiv = document.createElement('div');
+        newDiv.setAttribute('id', 'titleAboutMe');
+        aboutMeInfo.style.display = 'flex';
+    }
+});
 
-        }
-    })
-}
+
+//Navigation Links Contacts
+var navigationLinksContactsInfo = document.querySelector('#navigationLinkContacts');
+var contactsInfo = document.querySelector('.contactsInfo');
+navigationLinksContactsInfo.addEventListener('click', function () {
+    mainContent.style.display = 'none';
+    if (!newDiv) {
+        var newDiv = document.createElement('div');
+        newDiv.setAttribute('id', 'titleAboutMe');
+        contactsInfo.style.display = 'flex';
+    }
+});
+
+// }
+//
+// openProject();
+
+//Gallery Menu
+// var galleryMenuCategories = document.getElementsByClassName('menuCategories');
+// var galleryMenuPageBackground = document.getElementsByClassName('main_background')[0];
+// // debugger;
+//
+// var category;
+// for (var i = 0; i < galleryMenuCategories.length; i++) {
+//     category = galleryMenuCategories[i];
+//     var categoryMouseover = category.addEventListener('mouseover', function categoryInFocus() {
+//         var findLindCurrentCategory = this.getElementsByTagName('a')[0];
+//         findLindCurrentCategory.style.display = 'block';
+//         if(findLindCurrentCategory.id === 'section3D') {
+//             galleryMenuPageBackground.style.backgroundImage = 'url("../image/gallery_background_3d.png")';
+//         }
+//         else if(findLindCurrentCategory.id === 'sectionPainting') {
+//             galleryMenuPageBackground.style.backgroundImage = 'url("../image/gallery_background_painting.png")';
+//         }
+//         else {
+//             galleryMenuPageBackground.style.backgroundImage = 'url("../image/main_background.jpg")';
+//
+//         }
+//
+//     });
+//     var categoryMouseoout = category.addEventListener('mouseout', function categoryInFocus() {
+//         var findLindCurrentCategory = this.getElementsByTagName('a')[0];
+//         findLindCurrentCategory.style.display = 'none';
+//         if(findLindCurrentCategory.id === 'section3D') {
+//             galleryMenuPageBackground.style.backgroundImage = 'url("../image/main_background.jpg")';
+//
+//         }
+//         else if(findLindCurrentCategory.id === 'sectionPainting') {
+//             galleryMenuPageBackground.style.backgroundImage = 'url("../image/main_background.jpg")';
+//
+//         }
+//     })
+// }
 
 
 
